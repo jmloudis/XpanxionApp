c963ae4ecbb0898427099ee60aeb9d3666ccbabd
Jordan Loudis <jloudis@xpanxion.com>
2022-06-13 14:23:56 -0400

    create Hello World app and a Titlecase Converter GUI

---
+++ src/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.example.jframe.createGUI;
+
+import javax.swing.*;
+
+//class
+public class Main {
+
+    //method
+    public static void main(String[] args){
+
+        // statement
+        System.out.println("Hello World");
+
+        JFrame frame = createGUI.createJFrame();
+        frame.setVisible(true);
+
+    }
+
+
+}
