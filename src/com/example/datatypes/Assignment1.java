62a94ad2c5e581dfb357cab86cf341f513d7e97a
jmloudis <jloudis@xpanxion.com>
2022-06-14 17:25:05 -0400

    Added some spaces to make output easier to read

--- src/com/example/datatypes/Assignment1.java
+++ src/com/example/datatypes/Assignment1.java
@@ -7,18 +7,18 @@ public class Assignment1 {
     public static void calculateTime(){
 
         Scanner input = new Scanner(System.in);
-        System.out.print("enter number of weeks");
+        System.out.print("Enter number of weeks:");
 
         int week = input.nextInt();
 
         int day = week * 7;
-        System.out.println(day + "days");
+        System.out.println(day + " days");
         int hours = day * 24;
-        System.out.println(hours + "hours");
+        System.out.println(hours + " hours");
         int minutes = hours * 60;
-        System.out.println(minutes + "minutes");
+        System.out.println(minutes + " minutes");
         int seconds = minutes * 60;
-        System.out.println(seconds + "seconds");
+        System.out.println(seconds + " seconds");
 
 
 
